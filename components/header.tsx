d61091ebe9d9b3eefdfc2aef556500b354e4d4d5
kenechris2003 <kenechris2000@gmail.com>
2025-03-15 14:51:40 +0100

    update

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,6 @@
 'use client';
  
 
-import Image from 'next/image';
 import Link from 'next/link'
  
 export default function Page() {
@@ -11,7 +10,7 @@ export default function Page() {
             <div className="h-50 justify-between items-center px-4 sm:px-4 sm:w-5xl lg:px-6 pt-8">
                 <nav className="mr-40 flex justify-between items-center w-4xl px-1.5 sm:ml-20 md:px-2 md:ml-20 lg:px-3">
                     <div className="flex items-center ml-20">
-                        <Link href={"/"}> <Image src="/logo.png" alt="logo" className="h-16 w-80 lg:h-8 lg:w-auto" /> </Link>
+                        <Link href={"/"}> <img src="/logo.png" alt="logo" className="h-16 w-80 lg:h-8 lg:w-auto" /> </Link>
                         <ul className="hidden md:flex ml-20">
                             <li><Link href="/" className="px-2 py-2 text-sm font-medium rounded-md focus:outline-none focus:text-white focus:bg-gray-700 text-gray-300 hover:text-white hover:bg-gray-700">HOME</Link></li>
                             <li><Link href="/about" className="px-2 py-2 text-sm font-medium rounded-md focus:outline-none focus:text-white focus:bg-gray-700 text-gray-300 hover:text-white hover:bg-gray-700">CATEGORIES</Link></li>
